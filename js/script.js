aa1b0742c6c887bcbf0e54409d6cfb72751269a6
LautaroCasco <cascolautaroemanuel@gmail.com>
2024-12-26 15:30:33 -0300

    Basic Start screen

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// script.js
+function startGame() {
+    alert('¡El juego está comenzando!');
+    // Aquí puedes redirigir al usuario a la pantalla del juego o iniciar la lógica del juego.
+    // window.location.href = 'game.html';
+}
